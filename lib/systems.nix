4b3a647e5e2699bb81db4e50726c2328d17d5680
Phillip Cloud <cloud@standard.ai>
2021-03-21 11:33:36 -0400

    Add to systems.nix

--- lib/systems.nix
+++ lib/systems.nix
@@ -3,4 +3,5 @@
   i686-linux = "i686-unknown-linux-gnu";
   x86_64-darwin = "x86_64-apple-darwin";
   x86_64-linux = "x86_64-unknown-linux-gnu";
+  x86_64-unknown-linux-musl = "x86_64-unknown-linux-musl";
 }
